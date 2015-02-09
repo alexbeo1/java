2149cb3fd9764f498d7891fe0357439b4e0af18a
gkislin@yandex.ru <github13>
2015-02-09 23:30:05 +0300

    Add jsp

---
+++ src/ru/javawebinar/webapp/web/HtmlUtil.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.webapp.web;
+
+import ru.javawebinar.webapp.model.ContactType;
+import ru.javawebinar.webapp.model.Organization;
+import ru.javawebinar.webapp.model.Resume;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * User: gkislin
+ * Date: 06.03.14
+ */
+public class HtmlUtil {
+    public static String getContact(Resume r, ContactType type) {
+        String contact = r.getContact(type);
+        return contact == null ? "&nbsp;" : type.toHtml(contact);
+    }
+}
--- src/ru/javawebinar/webapp/web/ResumeServlet.java
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.webapp.web;
 
+import ru.javawebinar.webapp.model.Resume;
+import ru.javawebinar.webapp.storage.IStorage;
+import ru.javawebinar.webapp.storage.XmlFileStorage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,16 +15,42 @@ import java.io.Writer;
  * 09.02.2015.
  */
 public class ResumeServlet extends HttpServlet {
+    public static XmlFileStorage storage = new XmlFileStorage("C:\\Java\\webapp5\\file_storage");
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+/*
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         Writer w = response.getWriter();
         String name = request.getParameter("name");
-        w.write("Тест сервелет: привет " + name);
+        w.write("Тест сервелет: " + name);
         w.close();
+*/
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        Resume r;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                response.sendRedirect("list");
+                return;
+            case "create":
+                r = Resume.EMPTY;
+                break;
+            case "view":
+            case "edit":
+                r = storage.load(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        request.setAttribute("resume", r);
+        request.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(request, response);
     }
 }
