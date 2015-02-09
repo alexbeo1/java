f410b7de1e0fc7887a361470852281b4d8fac463
gkislin@yandex.ru <github13>
2015-02-09 21:24:41 +0300

    Add web.xml

---
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.webapp.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * GKislin
+ * 09.02.2015.
+ */
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer w = response.getWriter();
+        w.write("Тест сервелет: привет");
+        w.close();
+    }
+}
